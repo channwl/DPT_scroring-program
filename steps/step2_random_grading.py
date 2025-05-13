9f09c8755c497d8c5c69c9fdbecc2827078a4a7c
channwl <jason041128@naver.com>
2025-05-14 00:31:26 +0900

    Update step2_random_grading.py

--- steps/step2_random_grading.py
+++ steps/step2_random_grading.py
@@ -3,15 +3,11 @@
 
 import streamlit as st
 import random
+import tempfile
 from utils.pdf_utils import extract_text_from_pdf
 from utils.text_cleaning import clean_text_postprocess
 from utils.file_info import extract_info_from_filename
 from chains.grading_chain import grade_answer
-from utils.pdf_utils import extract_text_with_image_ocr
-
-
-import tempfile
-from utils.pdf_utils import extract_text_from_pdf
 
 def process_student_pdfs(pdf_files):
     answers, info = [], []
